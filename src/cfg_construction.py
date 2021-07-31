9df1c477fc53f6f638461aaf8dbe97756a15374c
Shemesh <mor.m.shemesh@gmail.com>
2021-07-31 18:05:49 +0300

    Working POC - tested on simple example

--- src/cfg_construction.py
+++ src/cfg_construction.py
@@ -115,19 +115,10 @@ class Block:
     jump_targets: Optional[list]
 
 
-def find_block_start(b: Block):
-    """
-
-    :param b: A block to find the start of
-    :return:
-    """
-    pass
-
-
 def build_cfg(variables: List[Var], instructions: List[Opcode]):
     pass
-    # log = make_logger(stdout=True, filepath="cfg_construction.log")
-    # log.info('New run!')
+    log = make_logger(stdout=True, filepath="cfg_construction.log")
+    log.info('New run!')
 
     jumps = defaultdict(list)
     blocks = dict({0: Block(start=0, end=-1, jump_targets=list())})  # Just a sorted dict of the start:end
@@ -136,31 +127,50 @@ def build_cfg(variables: List[Var], instructions: List[Opcode]):
     curr_block = blocks[0]
     last_jump_skipped = False
     for i, ins in enumerate(instructions):
-        # Create new block if I am jumped to, or someone jumped ahead of me
+        # Create new block if I am jumped to,
+        # or someone jumped ahead of me
         if i in jumps or last_jump_skipped:
             last_jump_skipped = False
             curr_block = Block(start=i, end=-1, jump_targets=list())
             blocks[i] = curr_block
             block_starts.add(i)
 
+        # We reached a Jump opcode that means something (jumps anywhere that's not one opcode ahead)
         if isinstance(ins, Jump) and ins.target != i + 1:
+            # Finish the current block
             curr_block.end = i
 
-            if ins.target > i:
-                jumps[ins.target].append(i)
-                curr_block.jump_targets.append(ins.target)
-            else:  # we need to split a previous block (or the current one)
-                block_to_split = blocks[block_starts.bisect_left(ins.target)]
-                new_block = Block(start=ins.target, end=block_to_split.end, jump_targets=block_to_split.jump_targets)
-                block_to_split.end = ins.target - 1
-                block_to_split.jump_targets = list(ins.target)
-                blocks[ins.target] = new_block
+            # If jump ahead -
+            jumps[ins.target].append(i)
+            curr_block.jump_targets.append(ins.target)
 
+            if ins.target < i:  # we need to split a previous block (or the current one)
+                log.debug(f"Splitting block {block_starts.bisect_left(ins.target) - 1}")
+                block_to_split = blocks[block_starts.bisect_left(ins.target) - 1]
+                new_block = Block(start=ins.target, end=block_to_split.end, jump_targets=block_to_split.jump_targets)
+                block_starts.add(ins.target)
+                block_to_split.end = ins.target - 1
+                block_to_split.jump_targets = [ins.target]
+                blocks[ins.target] = new_block
+                log.debug(f"Split block: {block_to_split}")
+                log.debug(f"New block: {new_block}")
+                curr_block = new_block
+
+            # if conditional jump - add the default continue ahead jump.
             if ins.condition:
                 curr_block.jump_targets.append(i + 1)
                 jumps[i + 1].append(i)
-            else:
+            else: # if we have a jump ahead with no default continue - we start a new block
                 last_jump_skipped = True
+        # Print the structures
+        log.debug(blocks)
+        log.debug(jumps)
+        log.debug(block_starts)
 
 
+def main():
+    build_cfg(None, code)
 
+
+if __name__ == "__main__":
+    main()
